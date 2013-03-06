53b96cd36c3fd52c260c28bf41224aac9c8ca883
Sandy Phan <San@San-Laptop>
2013-03-06 22:49:59 +1100

    Finished Exercises 2 and 3 for lab1

---
+++ Lab1/src/Greeter.java
@@ -0,0 +1,36 @@
+/**
+   A class for producing simple greetings.
+*/
+
+public class Greeter {
+   /**
+      Constructs a Greeter object that can greet a person or 
+      entity.
+      @param aName the name of the person or entity who should
+      be addressed in the greetings.
+   */
+   public Greeter(String aName) {
+      name = aName;
+   }
+
+   /**
+      Greet with a "Hello" message.
+      @return a message containing "Hello" and the name of
+      the greeted person or entity.
+   */
+   public String sayHello() {
+      return "Hello, " + name + "!";
+   }
+   
+   /**
+    * Sets the name of the object to the input name of a person
+    * or entity to be greeted.
+    * @param aName the name of the person or entity who should
+    * be addressed in the greetings.
+    */
+   public void setName(String aName) {
+	   name = aName;
+   }
+
+   private String name;
+}
