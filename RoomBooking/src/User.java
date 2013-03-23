738fead4c18ca9d2344e193b4dc92d1594e3500d
sphan <santhai.phan@gmail.com>
2013-03-23 20:57:25 +1100

    Added a list of reservation for each user.

--- RoomBooking/src/User.java
+++ RoomBooking/src/User.java
@@ -1,18 +1,31 @@
+import java.util.LinkedList;
 
 public class User {
 	private String name;
+	private LinkedList<Reservation> reservations = new LinkedList<Reservation>();
 	
 	public User(String aName) {
 		setName(aName);
 	}
 
+	/**
+	 * Get the name of the current user.
+	 * @return A String that specifies the name of the
+	 * current user.
+	 */
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
+	/**
+	 * Set the name of the user.
+	 * @param name A String that specifies the name of the user.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
-	
+	public void addReservation() {
+		
+	}
 }
