16c62aadd3ce121e20a0ee92889cc1a22a28e19f
sphan <santhai.phan@gmail.com>
2013-03-23 20:54:28 +1100

    Implemented set reservation time, title and room by finding a room with a fitting capacity.

--- RoomBooking/src/Reservation.java
+++ RoomBooking/src/Reservation.java
@@ -7,26 +7,62 @@ public class Reservation {
 
 	private static final int DAYS_IN_WEEKS = 7;
 	private String title;
-	private Date start;
-	private Date end;
-	private Calendar startDate;
-	private Calendar endDate;
+	private Calendar startDate, endDate;
+	private Calendar startTime, endTime;
 	private User user;
 	private Room room;
 	
-	public Reservation(String userName, int capacity, int numWeeks, String month, int date, int time, int duration, String title) {
+	public Reservation() {
+		
 	}
 	
-	public void setDates(int month, int date, int numWeeks) {
-		startDate = Calendar.getInstance();
-		startDate.set(Calendar.MONTH, month);
-		startDate.set(Calendar.DATE, date);
+	/**
+	 * Set the dates of reservation for the duration.
+	 * @param month An integer that specifies the month of the year.
+	 * @param date An integer that specifies the date of the month.
+	 * @param numWeeks An integer that specifies the number of weeks
+	 * the reservation will be going for.
+	 */
+	public void setReservationDates(int month, int date, int numWeeks) {
+		this.startDate = Calendar.getInstance();
+		this.startDate.set(Calendar.MONTH, month);
+		this.startDate.set(Calendar.DATE, date);
 		int numberOfDays = DAYS_IN_WEEKS * numWeeks;
-		endDate = startDate;
-		endDate.add(Calendar.DATE, numberOfDays);		
+		this.endDate = startDate;
+		this.endDate.add(Calendar.DATE, numberOfDays);
 	}
 	
+	/**
+	 * Set the time of reservation.
+	 * @param time An integer that specifies the start time of the reservation.
+	 * @param duration An integer that specifies the number of hours.
+	 */
+	public void setReservationTime(int time, int duration) {
+		this.startTime = Calendar.getInstance();
+		this.startTime.set(Calendar.HOUR_OF_DAY, time);
+		this.endTime.set(Calendar.HOUR_OF_DAY, startTime.get(Calendar.HOUR_OF_DAY) + duration);
+	}
 	
+	/**
+	 * Set the title of the reservation
+	 * @param title A string that defines the name of
+	 * the reservation.
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	/**
+	 * Get the title of the reservation
+	 * @return A string of the name of the reservation
+	 */
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public void setRoom(int capacity) {
+		this.room = room.findRoom(capacity);
+	}
 	/**
 	 * Verify if the room is reserved at a specific time or not.
 	 * @param room The room that needs to check its availability.
