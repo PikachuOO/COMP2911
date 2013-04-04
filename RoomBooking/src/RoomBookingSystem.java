7d727fcdce9a24312aaaf1955697d0bc765e2a2e
sphan <santhai.phan@gmail.com>
2013-04-04 13:51:31 +1100

    Added a 'print' case for the inputs.

--- RoomBooking/src/RoomBookingSystem.java
+++ RoomBooking/src/RoomBookingSystem.java
@@ -22,13 +22,9 @@ public class RoomBookingSystem {
 					break;
 				case "Book":
 					Reservation.createReservation(params);
-//					try {
-//						Reservation.createReservation(params);
-//					} catch (NumberFormatException e) {
-//						e.printStackTrace();
-//					} catch (Exception e) {
-//						System.out.println("Invalid inputs. Nothing was done");
-//					}
+					break;
+				case "Print":
+					Room.printReservations(params[1]);
 					break;
 				}
 			}
