5b83dbd4c167abd967a15aad421ece2671b02f33
sphan <santhai.phan@gmail.com>
2013-04-20 23:54:07 +1000

    Major changes! Create a completely new output file on start up. Made serveral classes to accept inputs according to the actions needed to take. Now use a book method that check through all possible solutions to book a reservation in any possible room. Also made a change method to create input instances.

--- RoomBooking/src/RoomBookingSystem.java
+++ RoomBooking/src/RoomBookingSystem.java
@@ -1,37 +1,68 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class RoomBookingSystem {
 	public static void main(String args[]) {		
 		try {
-			Scanner inputScanner = new Scanner(new File("src/inputs.txt"));
-//			Scanner inputScanner = new Scanner(new File(args[0]));
-//			File output = new File(args[1]);
+			Scanner inputScanner = new Scanner(new File(args[0]));
+			File output = new File(args[1]);
+			if (output.exists()) {
+				output.delete();
+			}
+			
 			while (inputScanner.hasNext()) {
 				String s = inputScanner.nextLine();
 				String[] params = s.split(" ");
 				if (params[0].equals("Room")) {
-					try {
-						Room.createRoom(Integer.parseInt(params[1]), params[2]);
-					} catch (NumberFormatException e) {
-						e.printStackTrace();
-					} catch (Exception e) {
-						System.out.println("Sorry, room added. Hence, skipped.");
-					}
+					RoomInputs inputs = new RoomInputs(params);
+					Room.createRoom(inputs, output);
 				} else if (params[0].equals("Book")) {
-					Reservation.createReservation(params);
+					book(params, output);
 				} else if (params[0].equals("Change")) {
-					Reservation.changeReservation(params);
+					change(params, output);
 				} else if (params[0].equals("Delete")) {
-					Reservation.deleteReservation(params);
+					DeleteInputs inputs = new DeleteInputs(params);
+					Reservation.deleteReservation(inputs, output);
 				} else if (params[0].equals("Print")) {
-					Printer.printReservations(params[1]);
+					Printer.printReservations(params[1], output);
 				}
 			}
 			inputScanner.close();
+			Printer.closeFile();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private static void book(String[] params, File output) {
+		ListIterator<Room> i = Room.getAllRooms().listIterator();
+		Room room;
+		BookingInputs inputs = new BookingInputs(params);
+		
+		while (i.hasNext()) {
+			room = i.next();
+			if(room.getCapacity() >= inputs.getCapacity()) {
+				if(!room.foundReservations(inputs.getMonth(), 
+						inputs.getDate(), inputs.getTime(), inputs.getNumWeeks())) {
+					Reservation.createReservation(inputs, output);
+					break;
+				}
+			}
+		}
+	}
+	
+	private static void change(String[] params, File output) {
+		ChangeInputs cInputs= new ChangeInputs(params);
+		String[] deleteParams = {"Delete", params[1], params[2],
+				params[3], params[4], params[5], params[6]};
+		DeleteInputs dInputs  = new DeleteInputs(deleteParams);
+		String[] bookingParams = {"Book", params[1], params[7],
+				params[3], params[8], params[9], params[10], 
+				params[11], params[12]};
+		BookingInputs bInputs = new BookingInputs(bookingParams);
+		
+		Reservation.changeReservation(cInputs, dInputs, bInputs, output);
 	}
 }
